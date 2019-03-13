bf496e9b70d2a559bc0997c21f3c21058c051b65
JiaLuenTeo <jialuenteo@gmail.com>
2019-03-13 15:56:11 +0800

    VER 0.25 HALF IMPLEMENTED PLAYER GUN AIMING

--- Assets/Scripts/PlayerFollowMouse.cs
+++ Assets/Scripts/PlayerFollowMouse.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerFollowMouse : MonoBehaviour
 {
+    public Transform gun;
+    public Transform playerCharacter;
+    public float speed = 10.0f;
+    Vector3 mousePosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //mousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        //float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        //gun.position = Vector3.Lerp(gun.position, mousePosition, speed);
+        //gun.RotateAround(playerCharacter.position, gun.forward, angle );
+
+        Vector3 orbVector = Camera.main.WorldToScreenPoint(playerCharacter.position);
+        orbVector = Input.mousePosition - orbVector;
+        float angle = Mathf.Atan2(orbVector.y, orbVector.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+
+        gun.position = playerCharacter.position + new Vector3(0.2f,0);
+        gun.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
