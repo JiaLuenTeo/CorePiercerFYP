46411a38e01d656448cadfb5e8b4bcc4cbf85d62
OoiYeeSoon <011159@kdu-online.com>
2019-05-17 17:26:11 +0800

    Branch_0.31
    Added UI for MainMenu as well as SceneManager to access to all pages:
    Start -> the game
    Options -> Optiona Menu
    Exit -> close game

---
+++ Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadOptions()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
