10d519e78703129b1317fc73b5571e7f1d9a561a
OoiYeeSoon <011159@kdu-online.com>
2019-06-14 17:47:21 +0800

    1. Added heart sprites in Player HP UI 2. Added time limit of 5 sec for the Ability Cooldown 3. Modified the BOSS HP UI with the filled image (still in the works)

---
+++ Assets/Scripts/AbilityCoolDown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityCoolDown : MonoBehaviour
+{
+    public Image abilityCD;
+    public float cooldown = 5;
+    public bool ready;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ready = true;
+        }
+
+        if (ready)
+        {
+            abilityCD.fillAmount += 1 / cooldown * Time.deltaTime;
+
+            if(abilityCD.fillAmount >= 1)
+            {
+                abilityCD.fillAmount = 0;
+                ready = false;
+            }
+        }
+    }
+}
